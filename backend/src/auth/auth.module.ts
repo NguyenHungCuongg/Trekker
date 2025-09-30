9010ce034950f631f5ec23dafb4992df847a455c
Nguyen Hung Cuong <cuonghungnguyentop@gmail.com>
2025-09-30 22:44:22 +0700

    Thiết lập lại prettier

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from 'src/user/user.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/user.entity';
-import { JwtModule } from '@nestjs/jwt';
-import { authConstants } from './constants/auth.constants';
+import { Module } from "@nestjs/common";
+import { UserModule } from "src/user/user.module";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "src/user/user.entity";
+import { JwtModule } from "@nestjs/jwt";
+import { authConstants } from "./constants/auth.constants";
 
 @Module({
   imports: [
@@ -13,7 +13,7 @@ import { authConstants } from './constants/auth.constants';
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: authConstants.secret,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: "1d" },
     }),
   ],
   controllers: [AuthController],
