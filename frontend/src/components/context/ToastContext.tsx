125d51361f290e892501d61734496471c0b7a498
Nguyen Hung Cuong <cuonghungnguyentop@gmail.com>
2025-10-24 13:51:55 +0700

    Fix lỗi rename 1

--- frontend/src/components/context/ToastContext.tsx
+++ frontend/src/components/context/ToastContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
-import Toast, { ToastType } from "../home/Toast";
+import Toast, { ToastType } from "../home-temp/Toast";
 
 interface ToastContextType {
   showToast: (type: ToastType, message: string) => void;
