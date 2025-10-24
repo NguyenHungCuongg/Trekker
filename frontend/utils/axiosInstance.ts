13626ae0740dfa1c93046f0c0ef249667e861b99
Nguyen Hung Cuong <cuonghungnguyentop@gmail.com>
2025-10-24 11:26:02 +0700

    Sửa lỗi String must be in <Text>

--- frontend/utils/axiosInstance.ts
+++ frontend/utils/axiosInstance.ts
@@ -1,23 +1,26 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { API_BASE_URL, API_TIMEOUT } from '@env';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_BASE_URL, API_TIMEOUT } from "@env";
 
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "http://192.168.1.178:3000",
   timeout: Number(API_TIMEOUT) || 10000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
-axiosInstance.interceptors.request.use(async config => {
-  const token = await AsyncStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, error => {
-  return Promise.reject(error);
-});
+);
 
 export default axiosInstance;
