b10eace2b41921387783ccc941bd4c603db22bbf
Nguyen Hung Cuong <cuonghungnguyentop@gmail.com>
2025-10-31 13:36:15 +0700

    Bổ sung các thuộc tính phân quyền vào Database

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -2,6 +2,7 @@ import { Exclude } from "class-transformer";
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { Booking } from "../booking/booking.entity";
 import { Review } from "../review/review.entity";
+import { UserRole } from "../common/enums";
 
 @Entity("users")
 export class User {
@@ -15,14 +16,25 @@ export class User {
   @Exclude()
   password: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, name: "full_name" })
   fullName: string;
 
-  @Column({ unique: true, nullable: true })
+  @Column({ unique: true })
   phone: string;
 
-  @Column({ unique: true, nullable: true })
+  @Column({ unique: true })
   email: string;
+
+  @Column({ type: "enum", enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @Column({
+    name: "profile_image",
+    type: "varchar",
+    length: 255,
+    nullable: true,
+  })
+  profileImage: string;
 
   @Column({
     name: "created_at",
